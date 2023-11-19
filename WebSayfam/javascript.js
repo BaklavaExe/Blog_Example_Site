570cb9dcfd4421b16553d55df71b1f3c56bcb48b
Süleyman Avan <slymnavan41@gmail.com>
2023-11-19 18:19:27 +0300

    My Basic Blog
    My basic example web site

--- WebSayfam/javascript.js
+++ WebSayfam/javascript.js
@@ -1,1 +1,1 @@
-alert("Mailiniz Gönderilmiştir. En Kısa Sürede Geri Dönüş Yapılcaktır.Teşekkürler.");
+alert("Your e-mail has been sent. Your email will be returned as soon as possible. Thanks(It doesn't actually work, I just did it for the example.)");
